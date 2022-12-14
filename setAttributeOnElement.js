97dea2adf2643aded1c2b2a5b2410606aeff127a
erlinghoff <>
2022-12-14 02:12:19 +0100

    Improve code by creating the attribute if it does not already excist.

--- setAttributeOnElement.js
+++ setAttributeOnElement.js
@@ -1,16 +1,20 @@
+// This function sets an attribute on a given element.
 const setAttributeOnElement = function (element, attribute, value) {
-  // Check if all required arguments are provided.
-  if (!element || !attribute || !value) {
-    throw new Error('Missing required argument(s)');
-  }
+	// Check if all required arguments are provided.
+	if (!element || !attribute || !value) {
+		// If any of the required arguments are missing, throw an error.
+		throw new Error("Missing required argument(s)");
+	}
 
-  // Check if the given attribute is valid for the given element.
-  if (!element.hasAttribute(attribute)) {
-    throw new Error(`Element does not have a ${attribute} attribute`);
-  }
+	// Check if the given attribute is valid for the given element.
+	if (!element.hasAttribute(attribute)) {
+		// If the attribute is not valid for the element, set it with an empty value.
+		element.setAttribute(attribute, "");
+	}
 
-  // Set the value of the attribute.
-  element.setAttribute(attribute, value);
-}; 
+	// Set the value of the attribute.
+	element.setAttribute(attribute, value);
+};
 
+// Export the function so it can be used by other modules.
 export default setAttributeOnElement;
