f9c8a3d6e16235429685f9668702300e10d73c89
erlinghoff <>
2023-08-18 08:55:50 +0200

    Refined getElementWidth function for better clarity and efficiency

--- getElementWidth.js
+++ getElementWidth.js
@@ -1,18 +1,7 @@
-const getElementWidth = function getElementWidth(qs) {
-  // This function returns the width of the first element that matches the provided query selector
-
-  // Check if the query selector is valid and return 0 if it isn't
-  if (!qs || typeof qs !== 'string') return 0;
-
-  // Get the first element that matches the query selector
-  const element = document.querySelector(qs);
-
-  // Check if the element exists and return 0 if it doesn't
-  if (!element) return 0;
-
-  // Return the element's width
-  return element.scrollWidth;
+const getElementWidth = (qs) => {
+    if (typeof qs !== 'string' || !qs) return 0;
+    const element = document.querySelector(qs);
+    return element ? element.scrollWidth : 0;
 };
 
-// Export the function as the default export
 export default getElementWidth;
